fea80e9b2dd9d126887a5d0b131921a990a370ec
jodan <jodan.pz@gmail.com>
2021-02-02 18:11:20 +0100

    dg

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -2,15 +2,20 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
+import { MyPluginContext } from '../components/TestUseData/MyPluginContext';
 import { Text, View } from '../components/Themed';
+import { PluginDataProvider } from '../hooks/context/PluginDataProvider';
 
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
+    <PluginDataProvider>
+      <MyPluginContext />
+    </PluginDataProvider>
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>Tab Two</Text>
+    //   <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    //   <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    // </View>
   );
 }
 
